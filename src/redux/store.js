c33a96b87c1b48aacb6ae9029e1edba71f0fbd34
Oleg Tomillo <oleg@Olegs-MacBook-Pro.local>
2020-06-07 21:05:27 -0700

    added data normalization changes

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
 import { createStore, applyMiddleware } from 'redux'
+import { persistStore } from 'redux-persist'
 import logger from 'redux-logger'
 
 import rootReducer from './root-reducer'
@@ -9,4 +10,6 @@ const store = createStore(rootReducer, applyMiddleware(
     ...middlewares
 ))
 
-export default store
+const persistor = persistStore(store)
+
+export { store, persistor }
