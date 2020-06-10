1fa1984f3ea160dcfb2664fa1d76756e1e5f286f
Oleg Tomillo <oleg@Olegs-MacBook-Pro.local>
2020-06-10 00:02:02 -0700

    refactored collection-overview component

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -4,7 +4,7 @@ import { createStructuredSelector } from 'reselect'
 
 import CollectionPreview from '../collection-preview/collection-preview.component'
 
-import { selectCollections } from '../../redux/shop/shop.selectors'
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors'
 
 import './collections-overview.styles.scss'
 
@@ -16,8 +16,11 @@ const CollectionOverview = ({ collections }) => (
     </div>
 )
 
+
+
+
 const mapStateToProps = createStructuredSelector({
-    collections: selectCollections
+    collections: selectCollectionsForPreview
 })
 
 export default connect(mapStateToProps)(CollectionOverview)
